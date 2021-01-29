10b6697b6f75949b5efee56f2ce212edd550ca55
jakoblumpkin <lumpkinjakobr@gmail.com>
2021-01-28 18:09:44 -0600

    added JavaScript

--- app.js
+++
@@ -1,1 +0,0 @@
-document.write("<p>Really Big Steaks</p>")
--- app1.js
+++ app1.js
@@ -1,8 +1,23 @@
-let answer=prompt('Is longhorn your favorite steak house?: ');
-
-if (answer=='yes'){
-    document.write('<h1>Longhorn is the best!</h1>')
+function question(){
+  let answer=prompt('Have you ever ate Longhorn Steak? yes or no: ');
+  if (answer=='yes' || answer=='Yes' || answer=='YES'){
+  document.write('<h1>I know you enjoyed it!</h1>');
+  }
+   else{
+  document.write('<h1>You should try it!</h1>');
 }
-else{
-    document.write('<h1>Longhorn is ok</h1>')
+}
+
+
+function Rating(){
+    let answer=prompt('Rate longhorn on a scale from 1 to 5: ');
+    if ((answer < 1) || (answer > 5)){
+      Rating();
+    }
+     else{
+       document.write("<h3>Your rating: </h3>")
+       for (let i=0; i<answer; i++){
+          document.write("<img height='20px' src='https://www.iconspng.com/images/gold-star/gold-star.jpg'/> ")
+       }
+  }
 }
